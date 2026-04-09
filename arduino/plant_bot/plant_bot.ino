6f3c76a612c5110fca561cdfe27b3a3707f41921
x-lone <mymai1xl0n3@gmail.com>
2026-04-09 16:56:25 -0500

    Implemented push-button sensor cycling and refactored state handling by replacing string-based logic with a byte-based system

--- arduino/plant_bot/plant_bot.ino
+++ arduino/plant_bot/plant_bot.ino
@@ -1,13 +1,12 @@
 #include <MD_MAX72xx.h>
 #include <SPI.h>
 #include "DHT.h"
-#include <string.h>
 
-#define HARDWARE_TYPE MD_MAX72XX::FC16_HW
-#define MAX_DEVICES 4
-#define CS_PIN 3
-#define CLK_PIN 4
-#define DIN_PIN 2
+#define BTNPIN 12
+const unsigned long debounceDelay = 50;
+unsigned long lastDebounceTime = 0;
+byte lastButtonState = HIGH;
+byte buttonState = HIGH;
 
 #define DHTPIN 9
 #define DHTTYPE DHT11
@@ -15,14 +14,20 @@ DHT dht(DHTPIN, DHTTYPE);
 
 #define SOILPIN A0
 
+#define HARDWARE_TYPE MD_MAX72XX::FC16_HW
+#define MAX_DEVICES 4
+#define CS_PIN 3
+#define CLK_PIN 4
+#define DIN_PIN 2
+
 MD_MAX72XX matrix = MD_MAX72XX(HARDWARE_TYPE, DIN_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
 
-byte A[6] = {0b00000000, 0b01000010, 0b00001000, 0b10000001, 0b01111110, 0b00000000};
+const byte A[6] PROGMEM = {0b00000000, 0b01000010, 0b00001000, 0b10000001, 0b01111110, 0b00000000};
 
 #define DIGIT_W 3
 #define DIGIT_H 4
 #define DIGIT_SPACING (DIGIT_W + 1)
-byte digit_bitmaps[10][4] {
+const byte digit_bitmaps[10][4] PROGMEM = {
   {0b111, 0b101, 0b101, 0b111},
   {0b110, 0b010, 0b010, 0b111},
   {0b111, 0b001, 0b110, 0b111},
@@ -38,7 +43,7 @@ byte digit_bitmaps[10][4] {
 #define LETTER_W 3
 #define LETTER_H 3
 #define LETTER_SPACING (LETTER_W + 1)
-byte letter_bitmaps[26][3] {
+const byte letter_bitmaps [26][3] PROGMEM = {
   {0b010, 0b111, 0b101},
   {0b110, 0b111, 0b111},
   {0b111, 0b100, 0b111},
@@ -67,17 +72,56 @@ byte letter_bitmaps[26][3] {
   {0b110, 0b010, 0b011}
 };
 
-char current_sensor[4] = "TEMP"; 
+const unsigned long interval = 1000;
+unsigned long lastUpdate = 0;
+
+enum SensorType { TEMP, HUM, SOIL };
+byte current_sensor = TEMP;
 char current_value[12];
+
+float getTemperature() {
+  for (int i = 0; i < 3; i++) {
+    float temp = dht.readTemperature();
+    if (!isnan(temp)) return temp;
+  }
+
+  return -999;
+}
+
+int getHumidity() {
+  for (int i = 0; i < 3; i++) {
+    float hum = dht.readHumidity();
+    if (!isnan(hum)) return hum;
+  }
+
+  return -1;
+}
+
+int getSoil() {
+  return analogRead(SOILPIN);
+}
+
+void sendSensorValues() {
+  float temp = getTemperature();
+  float hum = getHumidity();
+  int soil = getSoil();
+
+  Serial.print("RETURNED:");
+  Serial.print(temp);
+  Serial.print(",");
+  Serial.print(hum);
+  Serial.print(",");
+  Serial.println(soil);
+}
 
 int pixelOffset(int size, int pos, bool flip) {
   return flip ? (size - 1 - pos) : pos;
 }
 
-void drawBitmap(byte *image, int image_width, int image_height, int xPos, int yPos) {
+void drawBitmap(const byte *image, int image_width, int image_height, int xPos, int yPos) {
   for (int row = 0; row < image_height; row++) {
     for (int col = 0; col < image_width; col++) {
-      bool pixel = bitRead(image[row], col);
+      bool pixel = (pgm_read_byte(&image[row]) >> col) & 0x01;
 
       int x = pixelOffset(image_width, col, true) + xPos;
       int y = pixelOffset(image_height, row, true) + yPos;
@@ -87,7 +131,7 @@ void drawBitmap(byte *image, int image_width, int image_height, int xPos, int yP
   }
 }
 
-void drawNumber(char *num, int x, int y) {
+void drawNumber(const char *num, int x, int y) {
   int xOff = x;
 
   while (*num) {
@@ -104,7 +148,7 @@ void drawNumber(char *num, int x, int y) {
   }
 }
 
-void drawWord(char *let, int x, int y) {
+void drawWord(const char *let, int x, int y) {
   int xOff = x;
 
   while (*let) {
@@ -117,24 +161,6 @@ void drawWord(char *let, int x, int y) {
   }
 }
 
-void sendSensorValues() {
-  float temp = dht.readTemperature();
-  float hum = dht.readHumidity();
-  int soil = analogRead(SOILPIN);
-
-  if (isnan(temp) || isnan(hum)) {
-    Serial.println("ERR,DHT_FAIL");
-    return;
-  }
-
-  Serial.print("RETURNED:");
-  Serial.print(temp);
-  Serial.print(",");
-  Serial.print(hum);
-  Serial.print(",");
-  Serial.println(soil);
-}
-
 void setup() {
   Serial.begin(9600);
 
@@ -143,13 +169,49 @@ void setup() {
   matrix.clear();
   drawBitmap(A, 8, 6, 0, 0);
 
+  pinMode(BTNPIN, INPUT_PULLUP);
+
   dht.begin();
 
   Serial.println("READY");
 }
 
+void handleButton() {
+  int reading = digitalRead(BTNPIN);
+
+  if (reading != lastButtonState) {
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (reading != buttonState) {
+      buttonState = reading;
+
+      if (buttonState == LOW) {
+        current_sensor = (current_sensor + 1) % 3;
+
+        lastUpdate = millis();
+        update();
+        draw();
+      }
+    }
+  }
+
+  lastButtonState = reading;
+}
+
 void update() {
-  dtostrf(dht.readTemperature(), 4, 2, current_value);
+  switch (current_sensor) {
+    case 0:
+      dtostrf(getTemperature(), 4, 2, current_value);
+      break;
+    case 1:
+      itoa(getHumidity(), current_value, 10);
+      break;
+    case 2:
+      itoa(getSoil(), current_value, 10);
+      break;
+  }
 }
 
 void draw() {
@@ -157,15 +219,24 @@ void draw() {
 
   matrix.clear();
 
-  drawWord(current_sensor, 0, 5);
+  switch (current_sensor) {
+    case TEMP: drawWord("TEMP", 0, 5); break;
+    case HUM: drawWord("HUM", 0, 5); break;
+    case SOIL: drawWord("SOIL", 0, 5); break;
+  }
+
   drawNumber(current_value, 0, 0);
 
   matrix.control(MD_MAX72XX::UPDATE, MD_MAX72XX::ON);
 }
 
 void loop() {
-  update();
-  draw();
+  handleButton()
 
-  delay(1000);
+  if (millis() - lastUpdate >= interval) {
+    lastUpdate = millis();
+
+    update();
+    draw();
+  }
 }
